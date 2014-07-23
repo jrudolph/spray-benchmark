2ef10457a96227a42d872bb1dc58431259be5018
Johannes Rudolph <johannes.rudolph@gmail.com>
2014-07-23 11:26:38 +0200

    some diagnostic info

--- benchmark.sh
+++ benchmark.sh
@@ -1,1 +1,8 @@
+# display some config variables
+sysctl net.ipv4.tcp_max_syn_backlog
+sysctl net.ipv4.tcp_tw_reuse
+sysctl net.ipv4.tcp_tw_recycle
+sysctl net.core.somaxconn
+ulimit -a
+
 ab -c 4000 -n 15000000 http://127.0.0.1:8080/plaintext
